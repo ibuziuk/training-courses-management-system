6c325e9d82eeaca234041a836e091dd2c5d369c8
GennadyT <gennady.trubach@mail.ru>
2015-07-16 13:03:41 +0300

    TCMS-18: Controllers created

---
+++ src/main/java/org/exadel/training/controller/AllTrainingsController.java
@@ -0,0 +1,14 @@
+package org.exadel.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/allTrainings")
+public class AllTrainingsController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String allTrainings() {
+        return "allTrainings";
+    }
+}

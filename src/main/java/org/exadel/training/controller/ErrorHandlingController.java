9eb9329679a3b0aa5db43f311ddb64e46bfacb6d
Alex <alexey_hww@tut.by>
2015-07-17 11:05:12 +0300

    TCMS-25: Generic error pages created

---
+++ src/main/java/org/exadel/training/controller/ErrorHandlingController.java
@@ -0,0 +1,25 @@
+package org.exadel.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ErrorHandlingController {
+    @RequestMapping(value = "error", method = RequestMethod.GET)
+    public String notFound(HttpServletRequest request, Model model)
+    {
+        model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status_code"));
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        String errorMessage = null;
+        if (throwable != null) {
+            errorMessage = throwable.getMessage();
+        }
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
+    }
+}

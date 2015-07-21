e5c296e9c15615536df08ff018f1547651a90204
Alex <alexey_hww@tut.by>
2015-07-21 12:12:01 +0300

    TMCS-13: EmailNotificationService ready to use

--- src/main/java/org/exadel/training/controller/LoginController.java
+++ src/main/java/org/exadel/training/controller/LoginController.java
@@ -1,18 +1,13 @@
 package org.exadel.training.controller;
 
-import org.exadel.training.service.EmailNotifierService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.context.Context;
 
 @Controller
 public class LoginController {
-    @Autowired
-    private EmailNotifierService emailNotifierService;
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
         return "redirect:/welcome";
@@ -23,9 +18,6 @@ public class LoginController {
         if (loginError != null) {
             model.addAttribute("loginError", true);
         }
-        String[] mails = {"yaroshevich.yana@gmail.com","phantom.rvr@gmail.com","toshabely@gmail.com","alexey_hw@tut.by","shchaurouski.slava@gmail.com","gennady.trubach@mail.ru"};
-        Context ctx = new Context();
-        emailNotifierService.sendEmailNotification(mails, "login", ctx);
         return "login";
     }
 }

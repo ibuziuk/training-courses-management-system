f837ca657d16547430c1d100b3cdda527794ae4f
slawiko <shchaurouski.slava@gmail.com>
2015-07-22 18:10:33 +0300

    TCMS-47: Get request support

--- src/main/java/org/exadel/training/controller/rest/CalendarController.java
+++ src/main/java/org/exadel/training/controller/rest/CalendarController.java
@@ -4,15 +4,15 @@ import org.exadel.training.model.Training;
 import org.exadel.training.service.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 public class CalendarController {
     @Autowired
     TrainingService trainingService;
-
-    @RequestMapping(value = "/q")
+    @RequestMapping(value = "/rest/calendar")
     public List<Training> getUsers(){
         return trainingService.getAllTraining();
     }

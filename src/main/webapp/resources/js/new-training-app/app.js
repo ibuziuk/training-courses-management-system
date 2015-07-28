393a278ef7029f74de5b15678d37eb55c94d6afa
GennadyT <gennady.trubach@mail.ru>
2015-07-28 12:49:40 +0300

    TCMS-55: Merge with pages done

--- src/main/webapp/resources/js/new-training-app/app.js
+++ src/main/webapp/resources/js/new-training-app/app.js
@@ -1,3 +1,7 @@
 'use strict';
+<<<<<<< HEAD
 
 angular.module('newTrainingApp', ['collapseCtrl', 'ui.bootstrap', 'ngNotify']);
+=======
+var newTrainingApp = angular.module('newTrainingApp', ['collapseCtrl', 'paginationCtrl', 'pageCtrl', 'ui.bootstrap']);
+>>>>>>> pages
--- src/main/webapp/resources/js/training-app/app.js
+++ src/main/webapp/resources/js/training-app/app.js
@@ -1,2 +1,6 @@
 'use strict';
+<<<<<<< HEAD
 angular.module('trainingApp', ['collapseCtrl', 'pageCtrl', 'ui.bootstrap']);
+=======
+angular.module('trainingApp', ['collapseCtrl', 'ui.bootstrap']);
+>>>>>>> pages
